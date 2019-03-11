97ea03037e0635b996b1f6c29820310e9126f154
Jaype27 <jaypee.0927@gmail.com>
2019-03-11 17:19:22 -0400

    enemy wall detect

--- Assets/Scripts/GroundEnemyController.cs
+++ Assets/Scripts/GroundEnemyController.cs
@@ -8,6 +8,11 @@ public class GroundEnemyController : MonoBehaviour {
 	public float _rayDistance;
 	private bool _isRight;
 	public Transform _groundSpot;
+
+	public Transform _wallSpot;
+	public float _wallCheckRadius;
+	public LayerMask _isWall;
+	private bool _hitWall;
 	
 
 	// Use this for initialization
@@ -17,8 +22,10 @@ public class GroundEnemyController : MonoBehaviour {
 	void Update () {
 		transform.Translate(Vector2.right * -_enemySpeed * Time.deltaTime);
 
+		_hitWall = Physics2D.OverlapCircle(_wallSpot.position, _wallCheckRadius, _isWall);
+
 		RaycastHit2D _groundDetect = Physics2D.Raycast(_groundSpot.position, Vector2.down, _rayDistance);
-		if(_groundDetect.collider == false) {
+		if(_groundDetect.collider == false || _hitWall) {
 			if(_isRight == true) {
 				transform.eulerAngles = new Vector3(0, 0, 0);
 				_isRight = false;
